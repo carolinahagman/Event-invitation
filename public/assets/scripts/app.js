6e67a3aa7c0a7fb1de02d51d84b04363b3f151e8
carolinahagman <hagmancarolina@gmail.com>
2021-03-15 20:35:13 +0100

    Add queryparams

--- public/assets/scripts/app.js
+++ public/assets/scripts/app.js
@@ -1,6 +1,38 @@
-let playBtn = document.querySelector(".play");
-let video = document.querySelector("#video");
+// Queryselectors
+const playBtn = document.querySelector(".play");
+const video = document.querySelector("#video");
+const personalTitle = document.querySelector("#personal-title");
+const nameField = document.querySelector("#name");
+const emailField = document.querySelector("#email");
+const addressField = document.querySelector("#address");
+const openHoursImg = document.querySelector("#open-hours-img");
+const contactImg = document.querySelector("#contact-img");
 
+// Query param values
+const urlParams = new URLSearchParams(window.location.search);
+const firstName = urlParams.get("firstname");
+const lastName = urlParams.get("lastname");
+const email = urlParams.get("email");
+const address = urlParams.get("address");
+
+// Populate buy-ticket form
+if (urlParams.has("firstname")) {
+  personalTitle.innerHTML = `Välkommen ${firstName}`;
+  if (urlParams.has("lastname")) {
+    nameField.value = `${firstName} ${lastName}`;
+  } else {
+    nameField.value = firstName;
+  }
+}
+if (urlParams.has("email")) {
+  emailField.value = email;
+}
+
+if (urlParams.has("address")) {
+  addressField.value = address;
+}
+
+// Play video on click
 playBtn.addEventListener("click", (e) => {
   e.preventDefault();
   playBtn.classList.toggle("hidden");
@@ -8,6 +40,16 @@ playBtn.addEventListener("click", (e) => {
   video.play();
 });
 
-const urlParams = new URLSearchParams(window.location.search);
-const firstName = urlParams.get("firstname");
-const lastName = urlParams.get("lastname");
+// Toggle darkened on images
+contactImg.addEventListener("mouseover", (e) => {
+  contactImg.classList.remove("darkened");
+});
+contactImg.addEventListener("mouseout", (e) => {
+  contactImg.classList.add("darkened");
+});
+openHoursImg.addEventListener("mouseover", (e) => {
+  openHoursImg.classList.remove("darkened");
+});
+openHoursImg.addEventListener("mouseout", (e) => {
+  openHoursImg.classList.add("darkened");
+});
